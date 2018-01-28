2b2c03aa786adf94ed9bb16890da28ce6c37f18f
Sarthak Kohli <Sarthak.kohli.0.7@gmail.com>
2018-01-28 20:00:52 +0530

    Update New-headlines.py

--- Scrapping News/New-headlines.py
+++ Scrapping News/New-headlines.py
@@ -1,1 +1,29 @@
 
+import requests
+import re
+from lxml import html
+#str = raw_input("Enter Tags\n").split(" ")
+page = requests.get('https://timesofindia.indiatimes.com/')
+html_content = html.fromstring(page.content)
+news = html_content.xpath('//div/ul/li/a/text()')
+news1 = html_content.xpath('//div/ul/li/a/span/text()')
+news2 = html_content.xpath('//div/ul/li/span/a/text()')
+for i in range(len(str)):
+        print "Latest News Containing " +str[i]+ " is"
+        a = 0
+        for j in news:
+                if re.search(str[i], j , re.IGNORECASE):
+                        print j
+                        a += 1
+        for j in news1:
+                if re.search(str[i], j , re.IGNORECASE):
+                        print j
+                        a += 1
+        for j in news2:
+                if re.search(str[i], j , re.IGNORECASE):
+                        print j
+                        a += 1
+        if(a == 0):
+                print "No new Found For The Given Tag \n"
+        else :
+                print "\n"
