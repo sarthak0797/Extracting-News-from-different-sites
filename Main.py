5a772126817e824773ec179e6321d321a4b99277
Sarthak Kohli <Sarthak.kohli.0.7@gmail.com>
2018-02-03 01:12:17 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,13 +2,17 @@ import TOI as toi
 
 keys = raw_input("Enter Tags\n").split(" ")
 
+
+#print "News containg " +keys+ " is"
+
+
 for j in range(len(keys)) :
     
     #to store the news we are getting from Times Of India
     str = []
 
     #to get the news from Times Of India
-    str = toi.get_news(str,keys)
+    str = toi.get_news(str,keys[j])
 
     print "News contaning " +keys[j]+ " is:\n"
 
