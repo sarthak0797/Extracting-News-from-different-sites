4f9eff46c8b2aab0c222e293600148ec2f7ad818
Sarthak Kohli <Sarthak.kohli.0.7@gmail.com>
2018-02-03 01:11:52 +0530

    Update TOI.py

--- TOI.py
+++ TOI.py
@@ -2,7 +2,7 @@ import requests
 import re
 from lxml import html
 
-def get_news(str,keys):
+def get_news(str,keys1):
 
     page = requests.get('https://timesofindia.indiatimes.com/')
 
@@ -21,47 +21,46 @@ def get_news(str,keys):
 
     tags2 = html_content.xpath('//div/ul/li/span/a/@pg')
 
-    for i in range(len(keys)):
-        
-        for j in range(len(tags)):
+    for j in range(len(tags)):
 
-                if re.search(keys[i], tags[j] , re.IGNORECASE):
+            if re.search(keys1, tags[j] , re.IGNORECASE):
 
-                        if(j < len(news) and len(news[j]) > 9):
+                    if(j < len(news) and len(news[j]) > 20):
 
-                                str.append(news[j])
+                            str.append(news[j])
 
-                elif (j < len(news) and re.search(keys[i], news[j] , re.IGNORECASE)):
+            elif (j < len(news) and re.search(keys1, news[j] , re.IGNORECASE)):
 
-                        if(len(keys[i]) < len(news[j])):
+                    if(len(keys1) < len(news[j]) and len(news[j]) > 20):
 
-                                str.append(news[j])
+                            str.append(news[j])
 
-        for j in range(len(tags1)):
+    for j in range(len(tags1)):
 
-                if re.search(keys[i], tags1[j] , re.IGNORECASE):
+            if re.search(keys1, tags1[j] , re.IGNORECASE):
 
-                        if(j < len(news1)):
+                    if(j < len(news) and len(news[j]) > 20):
 
-                                str.append(news[j])
+                            str.append(news[j])
 
-                elif (j < len(news1) and re.search(keys[i], news1[j] , re.IGNORECASE)):
+            elif (j < len(news1) and re.search(keys1, news1[j] , re.IGNORECASE)):
 
-                        if(len(keys[i]) < len(news1[j])):
+                    if(len(keys1) < len(news1[j]) and len(news[j]) > 20):
 
-                                str.append(news[j])
+                            str.append(news[j])
 
-        for j in range(len(tags2)):
+    for j in range(len(tags2)):
 
-                if re.search(keys[i], tags2[j] , re.IGNORECASE):
+            if re.search(keys1, tags2[j] , re.IGNORECASE):
 
-                        if(j < len(news2)):
+                    if(j < len(news) and len(news[j]) > 20):
 
-                                str.append(news[j])
+                            str.append(news[j])
 
-                elif (j < len(news2) and re.search(keys[i], news2[j] , re.IGNORECASE)):
+            elif (j < len(news2) and re.search(keys1, news2[j] , re.IGNORECASE)):
 
-                        if(len(str[i]) < len(news2[j])):
+                    if(len(keys1) < len(news2[j]) and len(news[j]) > 20):
 
-                                str.append(news[j])
+                            str.append(news[j])
     return str
+
