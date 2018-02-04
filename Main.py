4a22f404aec215af3ddb3b543d34bdb642746364
Sarthak Kohli <Sarthak.kohli.0.7@gmail.com>
2018-02-05 00:32:35 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import TOI as toi
+import BBC as bbc
 
 keys = raw_input("Enter Tags\n").split(" ")
 
@@ -11,16 +12,23 @@ for j in range(len(keys)) :
     #to store the news we are getting from Times Of India
     str = []
 
-    #to get the news from Times Of India
-    str = toi.get_news(str,keys[j])
-
     print "News contaning " +keys[j]+ " is:\n"
 
     #printing the news recieved from Times Of India
     print "News from Times Of India\n"
 
+    #to get the news from Times Of India
+    str = toi.get_news(str,keys[j])
+
     for i in str:
 
         print i
 
+    str = []
+
     print "News From CNN\n"
+
+    str = bbc.get_news(str,keys[j])
+
+    for i in str:
+        print i
